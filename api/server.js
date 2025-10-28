c05945eb3e1adc0bfac15aff80d707e3793ceac2
Thaliios <cahuzac.p@gmail.com>
2025-10-28 15:49:32 +0100

    fix origin for CORS error

--- api/server.js
+++ api/server.js
@@ -7,68 +7,61 @@ const cookieParser = require("cookie-parser");
 
 const port = 5000;
 
-const Cors= require('cors')
+const Cors = require("cors");
 const app = express();
-
 
 app.use(cookieParser());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
-
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 // app.use((req, res, next) => {
- 
-//   const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  
 
-//   const originHeader = req.headers.origin || 'N/A'; 
-  
-//   const refererHeader = req.headers.referer || 'N/A'; 
+//   const clientIp = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
 
-//   console.log(`\n--- REQUÊTE ENTRANTE ---`);
-//   console.log(`[${new Date().toLocaleTimeString()}] ${req.method} ${req.originalUrl}`);
-//   console.log(`  IP Client: ${clientIp}`);
-//   console.log(`  Origin Web: ${originHeader}`);
-//   console.log(`  Referer: ${refererHeader}`);
-//   console.log(`------------------------`);
-  
-//   next(); 
+//   const originHeader = req.headers.origin || 'N/A';
+
+//   const refererHeader = req.headers.referer || 'N/A';
+
+//   console.log(`\n--- REQUÊTE ENTRANTE ---`);
+//   console.log(`[${new Date().toLocaleTimeString()}] ${req.method} ${req.originalUrl}`);
+//   console.log(`  IP Client: ${clientIp}`);
+//   console.log(`  Origin Web: ${originHeader}`);
+//   console.log(`  Referer: ${refererHeader}`);
+//   console.log(`------------------------`);
+
+//   next();
 // });
-const originsAllowed = [
-  process.env.APP_URL, 
-  process.env.APP_URL_, 
-  process.env.DEVELOPPEMENT_APP_URL_,
-  process.env.DEVELOPPEMENT_APP_URL,
-  process.env.LOCALHOST_APP_URL,
-  process.env.LOCALHOST_APP_URL_,
-  ].filter(Boolean);
- 
-  
-  
+
+// 1. DÉFINITION des options de production (valeur par défaut)
 var corsOptions = {
- 
-origin: function (origin, callback) {
+  origin: process.env.PROD_APP_URL,
+  optionsSuccessStatus: 200,
+  credentials: true,
+};
 
- if (!origin) return callback(null, true); 
 
- 
- if (originsAllowed.includes(origin)) {
+if (process.env.NODE_ENV === "development") {
+  const originsAllowed = [
+    process.env.APP_URL,
+    process.env.DEVELOPPEMENT_APP_URL,
+    process.env.LOCALHOST_APP_URL,
+  ].filter(Boolean); 
+  corsOptions.origin = function (origin, callback) {
+    if (!origin) return callback(null, true);
 
- callback(null, true);
- } else {
-
- console.log(`🚫 CORS bloqué pour l'origine: ${origin}`);
- callback(new Error('Non autorisé par CORS'), false);
- }
-},
-optionsSuccessStatus: 200,
-  credentials: true 
+    if (originsAllowed.includes(origin)) {
+      callback(null, true);
+    } else {
+      console.log(`🚫 CORS bloqué pour l'origine: ${origin}`);
+      callback(new Error("Non autorisé par CORS"), false);
+    }
+  };
 }
 
 app.use(Cors(corsOptions));
-app.use(routes); 
-app.listen(port, '0.0.0.0', () => {
+app.use(routes);
+
+app.listen(port, "0.0.0.0", () => {
   console.log(`Example app listening on ${port}`);
 });
-
